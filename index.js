d537e1a3ed4f15f70d6be8b7672d46ee49a299ae
Abdulaziz <163637373+Abdulaziz-Abdukhalimov@users.noreply.github.com>
2025-03-13 17:38:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+const form = document.querySelector("#submit-form");
+const ulAssigments = document.querySelector(".assigments");
+const input = document.querySelector(".file-input");
 
+document.addEventListener("DOMContentLoaded", function () {
+  loadAssignments();
+});
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  function createAssignmentLink(item, name) {
+    return `
+    <li> <a href="${item}" target="_blank">${name}</a> </li>
+  `;
+  }
+  const assigmentName = prompt("What is the name of your assignment?");
+  ulAssigments.insertAdjacentHTML(
+    "beforeend",
+    createAssignmentLink(input.value, assigmentName)
+  );
+  saveAssignment(input.value, assigmentName);
+  input.value = "";
+  input.focus();
+});
+
+function saveAssignment(text) {
+  let assignments = JSON.parse(localStorage.getItem("assignments")) || [];
+  assignments.push(text);
+  localStorage.setItem("assignments", JSON.stringify(assignments));
+}
+
+function loadAssignments() {
+  let assignments = JSON.parse(localStorage.getItem("assignments")) || [];
+  assignments.forEach(createAssignmentLink);
+}
